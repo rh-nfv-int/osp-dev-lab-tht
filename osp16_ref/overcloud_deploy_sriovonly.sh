3ee85b8acbab0f1f1d395b45d9314531af34b865
Saravanan KR <krsacme@users.noreply.github.com>
2021-02-16 18:03:10 +0530

    Created siorvonly deployment for shift on stack and aligned nic config files (#4)

---
+++ osp16_ref/overcloud_deploy_sriovonly.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PARAMS="$*"
+USER_THT="$HOME/osp16_ref"
+
+openstack overcloud roles generate -o $HOME/roles_data.yaml Controller ComputeSriov
+
+openstack overcloud deploy $PARAMS \
+    --templates \
+    --timeout 120 \
+    -r $HOME/roles_data.yaml \
+    -n $USER_THT/network_data.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-sriov.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
+    -e $USER_THT/environment.yaml \
+    -e $USER_THT/network-environment.yaml \
+    -e $USER_THT/network-environment-sriovonly.yaml \
+    -e $USER_THT/ml2-ovs-nfv.yaml \
+    -e $HOME/containers-prepare-parameter.yaml
+
